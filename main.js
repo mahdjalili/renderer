55bbfbe2fcd6c87c669dd2fbd6676788451d79c1
Mahdi <mahdiwise@gmail.com>
2024-12-10 16:37:04 +0330

    minor changes

--- main.js
+++ main.js
@@ -12,10 +12,13 @@ global.navigator = dom.window.navigator;
 import { renderTemplate, magicResize } from "./utils/utils.js";
 
 const templates = JSON.parse(fs.readFileSync("./templates/templates.json", "utf8"));
+const template = JSON.parse(fs.readFileSync("./templates/template.json", "utf8"));
+await renderTemplate(template, "template");
 
-for (let i = 0; i < templates.length; i++) {
-    const template = templates[i];
-    console.time(`${i + 1}/${templates.length}`);
-    await renderTemplate(template, `template-${i + 1}`);
-    console.timeEnd(`${i + 1}/${templates.length}`);
-}
+// for (let i = 0; i < templates.length; i++) {
+//     const template = templates[i];
+//     console.time(`${i + 1}/${templates.length}`);
+//     const resizedTemplate = await magicResize(template, 1080, 1920);
+//     await renderTemplate(resizedTemplate, `template-${i + 1}`);
+//     console.timeEnd(`${i + 1}/${templates.length}`);
+// }
