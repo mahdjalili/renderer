4fe04be5ca04c8134999aad5feee97d4fe2fc228
Mahdi <mahdiwise@gmail.com>
2025-01-01 12:50:13 +0330

    minor changes

--- src/middlewares/apiKey.middleware.ts
+++ src/middlewares/apiKey.middleware.ts
@@ -1,10 +1,10 @@
 import Elysia from "elysia";
+import { logger } from "../utils/logger";
 
 const VALID_API_KEYS = process.env.API_KEYS ? process.env.API_KEYS.split(",") : [];
 
 export const APIKeyMiddleware = new Elysia().onBeforeHandle({ as: "scoped" }, ({ headers, error }) => {
     const apiKey = headers["x-api-key"];
-    console.log(apiKey);
 
     if (!apiKey) {
         return error(401, { status: "error", message: "API Key is required" });
